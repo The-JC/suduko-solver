a19439258989aae2d31b6e115ff16b7e14488293
Julian Hellner <julian@hellner.cc>
2024-09-27 19:47:54 +0200

    Push local files

--- sudoku.py
+++ sudoku.py
@@ -441,6 +441,14 @@ class Sudoku:
 				s += str(self.matrix[y, x].toString()) + ' '
 			print(s)
 
+	def __str__(self):
+		s = ''
+		for y in range(0,9):
+			for x in range(0, 9):
+				s+= str(self.matrix[y, x].toString()) + ' '
+			s+= '\n'
+		return s
+
 class Cell:
 	def __init__(self,  x, y, value=0):
 		self.x = x
